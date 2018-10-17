00edf72098b6878908ee620b4fd88ec07085eaa2
Ingrid Cortes <ingridc957@gmail.com>
2018-10-17 14:01:25 -0500

    Con los cambio se tienen los datos necesarios para iniciar la simulación

--- main simulation line.rb
+++ main simulation line.rb
@@ -1,8 +1,16 @@
 # (rows_system, number_of_cashiers, simulation_time,
 #   duration_simulation_step)
+load 'structure simulation.rb'
+load 'users atributes.rb'
+load 'class random values.rb'
 
-  load 'users atributes.rb'
-  load 'class random values.rb'
 
-  users_info = Users_Atributes.new
-  puts users_info.make_users(10)
+# stage = Structure_Simulation.new(rows_system, number_of_cashiers, simulation_time, duration_simulation_step)
+stage = Structure_Simulation.new(1, 5, 19, 9)
+stage.set_stage
+puts "Debería mostrarme las filas #{stage.lines}"
+puts "Debería mostrarme el número de filas #{stage.lines.length}"
+puts "Debería mostrarme las cajas #{stage.cash_registers}"
+puts "Debería mostrarme el número de cajas #{stage.cash_registers.length}"
+puts "Debería mostrarme los usuarios"
+puts stage.users
